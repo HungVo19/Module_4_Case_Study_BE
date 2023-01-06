fa38dfe5fbf18f8e9b2c8c85d287a679cd34b8c9
LuuLeThaiSon <llts28497@gmail.com>
2023-01-06 10:07:30 +0700

    login by username or email

--- src/main/java/com/example/back_end/controller/LoginController.java
+++ src/main/java/com/example/back_end/controller/LoginController.java
@@ -5,10 +5,7 @@ import com.example.back_end.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
@@ -16,15 +13,29 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 
+//	@PostMapping
+//	public ResponseEntity<User> login(@RequestBody User user) {
+//		User userExist = userService.findUserByUsername(user.getUsername());
+//		if (userExist == null) {
+//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//		} else if (user.getPassword().equals(userExist.getPassword()) && userExist.isStatus()) {
+//			return new ResponseEntity<>(userExist, HttpStatus.OK);
+//		} else {
+//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//		}
+//	}
+
 	@PostMapping
-	public ResponseEntity<User> login(@RequestBody User user) {
-		User userExist = userService.findUserByUsername(user.getUsername());
-		if (userExist == null) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		} else if (user.getPassword().equals(userExist.getPassword()) && userExist.isStatus()) {
-			return new ResponseEntity<>(userExist, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	public ResponseEntity<User> login(@RequestParam("loginInput") String loginInput,
+									  @RequestParam("password") String password) {
+		User usernameLogin = userService.findUserByUsername(loginInput);
+		User emailLogin = userService.findUserByEmail(loginInput);
+		if (usernameLogin != null && usernameLogin.getPassword().equals(password)) {
+			return new ResponseEntity<>(usernameLogin, HttpStatus.OK);
 		}
+		if (emailLogin != null && emailLogin.getPassword().equals(password)) {
+			return new ResponseEntity<>(emailLogin, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 }
