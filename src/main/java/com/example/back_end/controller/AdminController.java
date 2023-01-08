52ea930553ffdc4c1df592c97efcec0e5f3c2106
LuuLeThaiSon <llts28497@gmail.com>
2023-01-08 13:45:22 +0700

    lay code

--- src/main/java/com/example/back_end/controller/AdminController.java
+++ src/main/java/com/example/back_end/controller/AdminController.java
@@ -5,7 +5,6 @@ import com.example.back_end.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -46,5 +45,6 @@ public class AdminController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<>(users, HttpStatus.OK);
+
 	}
 }
